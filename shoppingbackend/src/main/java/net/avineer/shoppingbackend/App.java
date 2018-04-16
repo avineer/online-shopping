62db19a7c7535b69cc2180da18dbe02580a20a08
Avi Neer <avineer@gmail.com>
2018-04-16 16:19:14 -0400

    My project setup

---
+++ shoppingbackend/src/main/java/net/avineer/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.avineer.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
